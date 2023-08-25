e1ec9f3b64171fdc20bbd234d78ea7fa3cec86d4
Roberto Cabral <roberto.cabral@peopleinteractive.com.br>
2023-08-25 16:57:43 -0300

    verificação se input tem somente numeros e adicionado separação de numeros

--- index.php
+++ index.php
@@ -13,11 +13,33 @@
 
     <form method="post" action="">
         <label for="cpf">CPF:</label>
-        <input type="number" id="cpf" name="cpf" maxlength="14" placeholder="Digite o CPF">
+        <input type="text" id="cpf" name="cpf" maxlength="14" placeholder="Digite o CPF" oninput="formatCPF(this)" oninput="allowOnlyNumbers(event)">
         <button type="submit">Verificar</button>
     </form>
 
     <?php require './includes/validar_cpf.php'; ?>
 </body>
+
+<script>
+    function allowOnlyNumbers(event) {
+        event.target.value = event.target.value.replace(/\D/g, '');
+    }
+    // Função para formatar o CPF com separadores
+    function formatCPF(input) {
+        // Remove todos os caracteres não numéricos
+        var value = input.value.replace(/\D/g, '');
+
+        // Formata o valor com os separadores
+        if (value.length > 3 && value.length <= 6) {
+            value = value.replace(/(\d{3})(\d{0,3})/, '$1.$2');
+        } else if (value.length > 6 && value.length <= 9) {
+            value = value.replace(/(\d{3})(\d{3})(\d{0,3})/, '$1.$2.$3');
+        } else if (value.length > 9) {
+            value = value.replace(/(\d{3})(\d{3})(\d{3})(\d{0,2})/, '$1.$2.$3-$4');
+        }
+
+        input.value = value;
+    }
+</script>
 
 </html>
